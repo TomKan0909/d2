80d37a32be55fb6dcf3d12d8e39aa84f2ea70a57
Sophy Sun <sophyzy.sun@mail.utoronto.ca>
2022-03-06 21:04:51 -0500

    dropdown added back in

---
+++ packages/components/src/lib/Containers/Dropdown/Dropdown.stories.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Meta, Story } from '@storybook/react';
+import { Button } from '../../Inputs/Button/Button';
+
+import { IDropdownProps } from './Dropdown';
+import Dropdown from './index';
+
+export default {
+    title: 'Components/Atoms/Dropdown',
+    component: Dropdown,
+    args: {
+        dropdownWidth: 300,
+        right: true,
+        dropdownButton: <Button>Click me</Button>,
+    },
+} as Meta;
+
+export const Basic: Story<IDropdownProps> = (args) => (
+    <Dropdown {...args}>
+        <Dropdown.Item>Settings</Dropdown.Item>
+        <Dropdown.Item>Help Center</Dropdown.Item>
+        <Dropdown.Item>Logout</Dropdown.Item>
+        <Dropdown.Item>Testing</Dropdown.Item>
+        <Dropdown.Item>Testing</Dropdown.Item>
+        <Dropdown.Item>Testing</Dropdown.Item>
+    </Dropdown>
+);
+export const Scrolling: Story<IDropdownProps> = (args) => (
+    <Dropdown {...args}>
+        <Dropdown.Item>Settings</Dropdown.Item>
+        <Dropdown.Item>Help Center</Dropdown.Item>
+        <Dropdown.Item>Logout</Dropdown.Item>
+        <Dropdown.Item>Testing</Dropdown.Item>
+        <Dropdown.Item>Testing</Dropdown.Item>
+        <Dropdown.Item>Testing</Dropdown.Item>
+        <Dropdown.Item>Testing</Dropdown.Item>
+        <Dropdown.Item>Testing</Dropdown.Item>
+        <Dropdown.Item>Testing</Dropdown.Item>
+        <Dropdown.Item>Testing</Dropdown.Item>
+        <Dropdown.Item>Testing</Dropdown.Item>
+    </Dropdown>
+);
